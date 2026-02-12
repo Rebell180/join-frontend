9a0bf3448e9ff81a2488839862675e10dbd875eb
Rebell180 <buchmann_marcel@web.de>
2026-02-12 09:10:44 +0100

    add default redirects to login page

--- join-frontend/src/app/app.routes.ts
+++ join-frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'login', pathMatch: 'full'},
+    {path: '**', redirectTo: 'login'},
+    {path: 'login', component: Login},
+];
